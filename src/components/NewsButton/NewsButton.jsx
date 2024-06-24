203f155c239119c26a5b9fa949e5de20695097d9
hoainam201 <mayurijudgement24@gmail.com>
2024-06-25 00:19:11 +0700

    Z index

--- src/components/NewsButton/NewsButton.jsx
+++ src/components/NewsButton/NewsButton.jsx
@@ -8,7 +8,7 @@ export default function NewsButton({name, image, id, subject}) {
   return (
     <button className="child shadow-lg hover:border-2 hover:border-blue-500" onClick={() => navigate(`/news/${id}`)}>
       <img src={image || news} alt={name} />
-      <div className="flex flex-col">
+      <div className="flex flex-col h-full justify-between">
         <p className="font-bold text-2xl text-blue-500 text-start mx-1">{name}</p>
         <p className="text-start mx-1 text-sm text-gray"><FormattedDate value={subject} year="numeric" month="long" day="numeric"/></p>
       </div>
